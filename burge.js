5dd1c0c5c2180dc7dd9951543bd74cb4c3689986
Sashank Pant <aadesh1337x@gmail.com>
2024-07-02 21:03:43 +0545

    javascript for hamburger

---
+++ burge.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    // Logic to toggle the menu visibility or perform other actions
+    // For example:
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('visible');
+}
