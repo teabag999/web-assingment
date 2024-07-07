75afd30108aef5f3c801720c6d5d21d503a343bc
Sashank Pant <aadesh1337x@gmail.com>
2024-07-07 10:29:41 +0545

    addition of features

--- burge.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    // Logic to toggle the menu visibility or perform other actions
-    // For example:
-    var menu = document.getElementById('menu');
-    menu.classList.toggle('visible');
-}
